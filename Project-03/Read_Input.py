611b7e4e09b7f993b4950b8491bcedd4de7a349b
Rajendra Kumar <raj.challenge.123@gmail.com>
2015-03-17 01:07:19 -0400

    Utility Functions added

--- Project-03/Read_Input.py
+++ Project-03/Read_Input.py
@@ -10,7 +10,7 @@ class readInputFile:
         self.taSkills={}
 
 
-    def generateFromInputFile(self):
+    def generateListsFromInputFile(self):
         f=open(self.file,'r')
         count=0
         for line in f:
@@ -27,33 +27,32 @@ class readInputFile:
             list.update({stringArr[0]:values})
 
     def getFunction(self,c):
-
         if(c==0):
             return self.getCourseTime()
-        if(c==1):
-            return self.getCourseRecite()
-        if(c==2):
-            return self.getCourseDet()
-        if(c==3):
-            return self.getCourseReq()
-        if(c==4):
-            return  self.getTAResp()
-        if(c==5):
+        elif(c==1):
+            return self.getCourseRecitations()
+        elif(c==2):
+            return self.getCourseDetails()
+        elif(c==3):
+            return self.getCourseRequirements()
+        elif(c==4):
+            return  self.getTAResponsibilities()
+        elif(c==5):
             return self.getTASkills()
 
     def getCourseTime(self):
         return self.courseTime
 
-    def getCourseRecite(self):
+    def getCourseRecitations(self):
         return self.courseRecite
 
-    def getCourseDet(self):
+    def getCourseDetails(self):
         return self.courseDet
 
-    def getCourseReq(self):
+    def getCourseRequirements(self):
         return self.courseReq
 
-    def getTAResp(self):
+    def getTAResponsibilities(self):
         return self.taResp
 
     def getTASkills(self):
@@ -62,50 +61,50 @@ class readInputFile:
     def printCourseTime(self):
         print self.courseTime
 
-if __name__ =='__main__':
-
-    # filename=raw_input()
-    # print "filename:",filename
-    filename = 'dataset_AI_CSP'
-    f=readInputFile(filename)
-    f.generateFromInputFile()
-
-    log=open("logfile.txt","w")
-
-    print "Course timings:"
-    print f.getCourseTime()
-
-    print "Course Recitations:"
-    print f.getCourseRecite()
-
-    print "Course Requirements:"
-    print f.getCourseReq()
-
-    print "Course Details:"
-    print f.getCourseDet()
-
-    print "TA Skills:"
-    print f.getTASkills()
-
-    print "TA Responsibilities:"
-    print f.getTAResp()
-
-    """
-    print >> log, "Course timings:"
-    print >> log, f.getCourseTime()
-
-    print >> log, "Course Recitations:"
-    print >> log, f.getCourseRecite()
-
-    print >> log,"Course Requirements:"
-    print >> log,f.getCourseReq()
-
-    print >> log, "Course Details:"
-    print >> log, f.getCourseDet()
-
-    print >> log, "TA Skills:"
-    print >> log, f.getTASkills()
-
-    print >> log, "TA Responsibilities:"
-    print >> log, f.getTAResp()
-    """
+# if __name__ =='__main__':
+#
+#     # filename=raw_input()
+#     # print "filename:",filename
+#     filename = 'dataset_AI_CSP'
+#     f=readInputFile(filename)
+#     f.generateFromInputFile()
+#
+#     log=open("logfile.txt","w")
+#
+#     print "Course timings:"
+#     print f.getCourseTime()
+#
+#     print "Course Recitations:"
+#     print f.getCourseRecite()
+#
+#     print "Course Requirements:"
+#     print f.getCourseReq()
+#
+#     print "Course Details:"
+#     print f.getCourseDet()
+#
+#     print "TA Skills:"
+#     print f.getTASkills()
+#
+#     print "TA Responsibilities:"
+#     print f.getTAResp()
+#
+#     """
+#     print >> log, "Course timings:"
+#     print >> log, f.getCourseTime()
+#
+#     print >> log, "Course Recitations:"
+#     print >> log, f.getCourseRecite()
+#
+#     print >> log,"Course Requirements:"
+#     print >> log,f.getCourseReq()
+#
+#     print >> log, "Course Details:"
+#     print >> log, f.getCourseDet()
+#
+#     print >> log, "TA Skills:"
+#     print >> log, f.getTASkills()
+#
+#     print >> log, "TA Responsibilities:"
+#     print >> log, f.getTAResp()
+#     """
--- Project-03/csp.py
+++ Project-03/csp.py
@@ -1,18 +1,124 @@
 import re
+import Read_Input
 
 class CSP:
+
     def __init__(self):
         self.assignment = []
         self.variables = []
+        self.constraints = []
+        self.courseTACount = {}
 
-    def updateValues(self):
-        inp = ReadInput('dataset_AI_CSP.txt')
-        self.variables = inp.getVariables()
+    def reset(self):
+        self.assignment = {}
+        self.variables = {}
+        # self.constraints = []
+        self.assignee = []
+        self.courseTACount = {}
+
+    def timeToNum(self, str):
+        ret = 0
+        list = str.split(':')
+        ampm = list.pop(-1).split(' ')
+        list.append(ampm[0])
+        if ampm[-1] == 'PM':
+            ret =  12*60
+        ret += (int(list[0])*60 + int(list[1]))
+        return ret
+
+    def timeRangeCheck(self, taTime, reciteTime):
+        """
+        This function checks whether there is any overlap in 90 mins of either times.
+        Returns True if they doesnt overlap else Returns False.
+
+        :param taTime:
+        :param reciteTime:
+        :return: True/False
+        """
+        if taTime == reciteTime: return False
+        if reciteTime > taTime and  reciteTime <= taTime + 90:
+            return False
+        elif taTime > reciteTime and  taTime <= reciteTime + 90:
+            return False
+        return True
+
+    def recitationConstraintCheck(self, ta, course, inp):
+        recitations = inp.getCourseRecitations()[course]
+        responsibilities = inp.getTAResponsibilities()[ta]
+        if recitations[0] != responsibilities[0]:
+            return True
+        taTime = self.timeToNum(recitations[1])
+        reciteTime = self.timeToNum(responsibilities[1])
+        return self.timeRangeCheck(taTime, reciteTime)
 
 
-def bactracking-search():
+    def skillTest(self, ta, course, inp):
+        """
+        This function identifies whether the TA matches all the course requirements.
+        :param ta:
+        :param course:
+        :param inp:
+        :return: True/False
+        """
+        taSkill = inp.getTASkills()
+        courseReq = inp.getCourseRequirements()
+        list = [x for x in taSkill[ta] if x not in courseReq[course]]
+        # print courseReq[course]
+        if len(list) != 0:
+            return False
+        return True
 
+    def isTARequired(self, course, inp):
+        """
+        Does the TA needs to attend the class
+        :return: True/False
+        """
+        cDet = inp.getCourseDetails()
+        if cDet[course][1] == 'yes':
+            return True
+        else:
+            return False
 
+    def computeTACntForClass(self, inp):
+        """
+        This function is responsible for computing how many TAs needed for each course.
+        :param inp: Read_Input.readInputFile
+        :return: void
+        """
+        cDet = inp.getCourseDetails()
+        for key in cDet.keys():
+            stud = cDet[key][0].strip(' ')
+            if stud >= 25 and stud < 40:
+                self.courseTACount[key] = 0.5
+            elif stud >= 40 and stud < 60:
+                self.courseTACount[key] = 1.5
+            elif stud >= 60:
+                self.courseTACount[key] = 2
+        # print self.courseTACount
 
-if __name__ = '__main__':
+    def updateValues(self, filename):
+        """
+        Generates all the basic data structures based on the input file.
+        :return: readInputFile object
+        """
+        inp = Read_Input.readInputFile(filename)
+        inp.generateListsFromInputFile()
+        # self.variables = inp.getTAResp().keys()
+        return inp
+
+    def bactracking_search(self):
+        print "help"
+
+    # def constraintCheck(self):
+
+if __name__ == '__main__':
     print ("Testing CSP.!")
+    obj = CSP()
+    # inp = obj.updateValues('dataset_AI_CSP')
+    # obj.computeTACntForClass(inp)
+    # print "Does TA1 has all the skills for RAJ? ", obj.skillTest('TA1','RAJ',inp)
+    print obj.timeRangeCheck(obj.timeToNum('2:30 PM'), obj.timeToNum('2:30 AM'))
+    print
+    # print " TAs Count: ", len(obj.variables)
+    # print
+    # print obj.timeToNum('2:30 PM')
